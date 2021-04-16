077f4cb29f28b7f17f6956a0e8fa605728cd14b3
Anthony Lopez <grungekarma@gmail.com>
2021-04-16 13:06:04 -0500

    starting the day one challenge

---
+++ day 2/day2.1.js
@@ -0,0 +1,3 @@
+const passwords = ["6-7 w: wwhmzwtwwk", "10-12 q: qqqqqqqqqqqqq", "16-17 d: ddddgdddddkddddsxddd", "2-4 q: shbqwqpps", "3-4 q: qkfq", "2-10 m: bkdrmcfdhmr", "13-19 s: blccvhsgsnsssqszssm", "1-3 l: qlxllhl", "4-6 h: hhhqhhh", "15-17 l: jrtllrlllllmmlllg", "11-12 n: bnzlnnnnbnntnnnc", "1-7 p: pppvppjppmptp", "5-6 t: tttnht", "8-12 g: glbnlqgggjkh"];
+
+
